78b0349dfd8047f64650ef509c312f658afa1e88
Rashi <roshkins@gmail.com>
2013-09-05 18:14:00 -0700

    Displayed events!

--- app/assets/javascripts/views/events/events_agenda_show.js
+++ app/assets/javascripts/views/events/events_agenda_show.js
@@ -2,8 +2,23 @@ Calendary.Views.EventsAgendaShow = Backbone.View.extend({
 	template: JST['events/agenda_show'],
 
 	render: function () {
-		var content = this.template({event: this.model});
+		var niceStart = this.niceTime(new Date(this.model.escape("start_time")));
+		var niceEnd   = this.niceTime(new Date(this.model.escape("end_time")));
+		var content = this.template({
+			event: this.model, 
+			niceStart: niceStart,
+			niceEnd: niceEnd
+		});
 		this.$el.html(content);
 		return this;
+	},
+
+	niceTime: function(dateObj) {
+
+		var AMorPM = ((dateObj.getHours() + 1)/(12) >= 1) ? "PM" : "AM";
+		var hour = ((dateObj.getHours()) % 12 === 0) ? 12 : dateObj.getHours() % 12;
+		var minute = (dateObj.getMinutes() < 10) ?
+		 "0" + dateObj.getMinutes() : dateObj.getMinutes();
+		return hour + ":" + minute + " " + AMorPM;
 	}
 });
